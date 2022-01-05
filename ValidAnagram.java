1f70b926e8697f75056ca87cacc6e3529170755a
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-01-05 19:11:13 +0530

    Create ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        
+      if(s.length() != t.length())
+            return false;
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        for(int i=0; i<str1.length; i++) {
+            if(str1[i] != str2[i])
+                return false;
+        }
+        return true;
+
+        
+        
+    }
+}
